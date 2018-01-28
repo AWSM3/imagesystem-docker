55e3d1e2753c9669776afb325c18dda8bf5016ec
Dmitry <dmitry2500u@bk.ru>
2018-01-28 04:35:56 +0300

    Клиент для взаимодействия с FileSystem API.

--- application/src/Api/FileSystem/Client.php
+++ application/src/Api/FileSystem/Client.php
@@ -12,11 +12,12 @@ namespace App\Api\FileSystem;
 use App\Api\FileSystem\Exception\BadResponseException;
 use App\Api\FileSystem\Exception\InvalidResponseJson;
 use App\Api\FileSystem\Interfaces\ClientInterface;
+use App\Api\FileSystem\Interfaces\GetFileResponseInterface;
 use App\Api\FileSystem\Interfaces\ResponseInterface;
+use App\Api\FileSystem\Method\GetFile;
 use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\RequestOptions;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Class Client
@@ -26,50 +27,52 @@ use Symfony\Component\Filesystem\Filesystem;
 class Client implements ClientInterface
 {
     /** @var HttpClient */
-    private $client;
+    private $httpClient;
     /** @var string FileSystem service base API url */
     private $baseUrl;
 
     /**
      * Client constructor.
      *
-     * @param HttpClient $client
+     * @param HttpClient $httpClient
      * @param ContainerInterface $container
      */
-    public function __construct(HttpClient $client, ContainerInterface $container)
+    public function __construct(HttpClient $httpClient, ContainerInterface $container)
     {
-        $this->client = $client;
-        $this->baseUrl = $container->getParameter('filesystem_url');;
+        $this->httpClient = $httpClient;
+        $this->baseUrl = $container->getParameter('filesystem_url');
     }
+
+    /**
+     * @return HttpClient
+     */
+    public function getHttpClient(): HttpClient
+    {
+        return $this->httpClient;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBaseUrl(): string
+    {
+        return $this->baseUrl;
+    }
+
 
     /**
      * @param string $id
      *
-     * @return ResponseInterface
+     * @return GetFileResponseInterface
      */
-    public function getFile(string $id): ResponseInterface
+    public function getFile(string $id): GetFileResponseInterface
     {
         $request = new Request($id);
+        $method = new GetFile($this);
 
-        $response = $this->client->get($this->baseUrl,
-            [
-                RequestOptions::QUERY => $request->getQuery()
-            ]
-        );
+        /** @throws \Exception */
+        $response = $method->makeRequest($request);
 
-        if ($response->getStatusCode() !== ResponseInterface::SUCCESS_CODE) {
-            throw new BadResponseException('Не получен требуемый ответ от файлового сервиса.', $response);
-        }
-
-        $responseContent = $response->getBody()->getContents();
-        try {
-            $responseArray = \GuzzleHttp\json_decode($responseContent, true);
-        } catch (\InvalidArgumentException $e) {
-            throw new InvalidResponseJson('Ответ файлового сервиса не является валидным JSON-объектом.', $response);
-        }
-
-        $responseObject = new Response($responseArray);
-
-        return $responseObject;
+        return $response;
     }
 }
