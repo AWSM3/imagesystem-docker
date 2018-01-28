4ce4131e3bc8a581e43d694e0f5b1e783057ae5a
Dmitry <dmitry2500u@bk.ru>
2018-01-28 04:38:01 +0300

    Эксепшен используемый при обращении к некорректному ключу массива data в ответе FileSystem API Response.

---
+++ application/src/Api/FileSystem/Exception/IncorrectDataItemException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author: AWSM3
+ * IncorrectDataItemException.php
+ */
+declare(strict_types=1);
+
+/** @namespace */
+namespace App\Api\FileSystem\Exception;
+
+/**
+ * Class IncorrectDataItemException
+ *
+ * @package App\Api\FileSystem\Exception
+ */
+class IncorrectDataItemException extends \RuntimeException
+{
+}
