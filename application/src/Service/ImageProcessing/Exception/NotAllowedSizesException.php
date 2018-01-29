74b289d944d983ea4c05809f73fed08812201a41
Dmitry <dmitry2500u@bk.ru>
2018-01-30 01:15:06 +0300

    Рефакторинг. Эксепшен о некорректных размерах изображения.

---
+++ application/src/Service/ImageProcessing/Exception/NotAllowedSizesException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Author: AWSM3
+ * ...description...
+ * NotAllowedSizes.php
+ */
+declare(strict_types=1);
+
+/** @namespace */
+
+namespace App\Service\ImageProcessing\Exception;
+
+/** @uses */
+
+/**
+ * Class NotAllowedSizes
+ *
+ * @package App\Service\ImageProcessing\Exception
+ */
+class NotAllowedSizes
+{
+
+}
